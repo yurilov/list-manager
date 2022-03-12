ddc0470fcb38cd34cced67f3cc20046125e28776
Yurii Shchepilov <yuraschepilov@gmail.com>
2022-03-12 13:07:32 +0200

    add button functionality

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import './sass/main.scss';
 import { addPairHandler } from '../src/js/addPairHandler.js';
 import { deleteBtnHandler } from '../src/js/deleteBtnHandler';
 import refs from './js/getRefs';
-console.log(refs.inputRef);
-window.addEventListener('click', e => {
+
+refs.formWrapRef.addEventListener('click', e => {
   e.preventDefault();
   addPairHandler(e);
   deleteBtnHandler(e);
--- src/js/addPairHandler.js
+++ src/js/addPairHandler.js
@@ -4,8 +4,18 @@ export function addPairHandler(e) {
   if (e.target.dataset.action !== 'add') {
     return;
   }
+  const value = refs.inputRef.value;
+  const splittedValue = value.split('=');
+  const pair = {
+    name: splittedValue[0].trim(),
+    value: splittedValue[1].trim(),
+  };
 
-  const pairToAdd = `<li class="pair-list__item">${refs.inputRef.value}</li>`;
+  const pairToAdd = `<li class="pair-list__item">
+  <span class="js-name">${pair.name}</span>
+  =
+  <span class="js-value">${pair.value}</span>
+  </li>`;
   refs.pairListRef.insertAdjacentHTML('beforeend', pairToAdd);
   refs.inputRef.value = '';
 }
